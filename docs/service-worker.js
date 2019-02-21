82ef5c60886e957cc00e1bd43fcff32ed95ec1fc
Jovica Aleksic <jovica.aleksic@xailabs.de>
2019-02-21 21:30:18 +0100

    use "homepage": "https://loopmode.github.io/stateful"

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70895c45c7e7a55ec9bff1c58fe6308b.js"
+  "/stateful/precache-manifest.48c360a2a0aea3652337f76d8b4d14ef.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/stateful/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
