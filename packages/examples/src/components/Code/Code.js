d7e007e4c63b79c3fcbf112888ce4c7e51fb5abc
Jovica Aleksic <jovica.aleksic@xailabs.de>
2019-02-28 11:58:29 +0100

    use libs/ chunks subfolder

--- packages/examples/src/components/Code/Code.js
+++ packages/examples/src/components/Code/Code.js
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 export const loadDependencies = async () => {
-    const Prism = await import(/* webpackChunkName: 'libs.prism' */ 'prismjs');
-    await import(/* webpackChunkName: 'libs.prism-theme-okaidia' */ 'prismjs/themes/prism-okaidia.css');
-    await import(/* webpackChunkName: 'libs.prism-bash' */ 'prismjs/components/prism-jsx');
-    await import(/* webpackChunkName: 'libs.prism-jsx' */ 'prismjs/components/prism-jsx');
-    const ReactPrism = await import(/* webpackChunkName: 'libs.prism-react' */ 'react-prism');
+    const Prism = await import(/* webpackChunkName: 'libs/prism' */ 'prismjs');
+    await import(/* webpackChunkName: 'libs/prism-theme-okaidia' */ 'prismjs/themes/prism-okaidia.css');
+    await import(/* webpackChunkName: 'libs/prism-bash' */ 'prismjs/components/prism-jsx');
+    await import(/* webpackChunkName: 'libs/prism-jsx' */ 'prismjs/components/prism-jsx');
+    const ReactPrism = await import(/* webpackChunkName: 'libs/prism-react' */ 'react-prism');
     return {
         Prism,
         ReactPrism
