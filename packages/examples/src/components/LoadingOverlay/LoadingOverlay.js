514489ac3e46ff356e184ed005df25a3e03ebd41
Jovica Aleksic <jovica.aleksic@xailabs.de>
2019-03-01 06:41:12 +0100

    use delay in LoadingOverlay

--- packages/examples/src/components/LoadingOverlay/LoadingOverlay.js
+++ packages/examples/src/components/LoadingOverlay/LoadingOverlay.js
@@ -5,9 +5,21 @@ import css from './LoadingOverlay.scss';
 
 import AnimatedLogo from 'components/AnimatedLogo';
 
-const LoadingOverlay = () => (
-    <div className={cx('LoadingOverlay', css.LoadingOverlay)}>
-        <AnimatedLogo scale={0.5} />
-    </div>
-);
+import useTimeout from '@loopmode/stateful/lib/hooks/timeout';
+import useDidMount from '@loopmode/stateful/lib/hooks/did-mount';
+import useWillUnmount from '@loopmode/stateful/lib/hooks/will-unmount';
+
+const LoadingOverlay = ({ delay = 500 }) => {
+    const [visible, setVisible] = React.useState(false);
+
+    const timer = useTimeout(() => setVisible(true), delay);
+    useDidMount(() => timer.start());
+    useWillUnmount(() => timer.clear());
+
+    return (
+        <div className={cx('LoadingOverlay', css.LoadingOverlay, { visible })}>
+            <AnimatedLogo scale={0.5} />
+        </div>
+    );
+};
 export default LoadingOverlay;
