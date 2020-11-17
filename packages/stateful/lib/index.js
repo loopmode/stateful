968eef643e986ef031c607cf674d99d1579ff63c
Jovica Aleksic <jovica.aleksic@etecture.de>
2020-11-17 16:04:57 +0100

    remove lib

--- packages/stateful/lib/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Stateful = void 0;
-var Stateful_1 = require("./Stateful");
-Object.defineProperty(exports, "Stateful", { enumerable: true, get: function () { return Stateful_1.Stateful; } });
