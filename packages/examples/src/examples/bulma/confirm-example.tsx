b8fb66e3314d32a799b9910ef25a3d5a1d24b89b
Jovica Aleksic <jovica.aleksic@etecture.de>
2020-12-02 09:42:09 +0100

    docs: update confirm examples

--- packages/examples/src/examples/bulma/confirm-example.tsx
+++ packages/examples/src/examples/bulma/confirm-example.tsx
@@ -9,32 +9,95 @@ const wait = (time: number) => new Promise((resolve) => setTimeout(resolve, time
 
 export default function ConfirmExample() {
   const handleProceed = async () => {
-    console.log("handleProceed");
-    await wait(2500);
-    console.log("handleProceed done");
+    await wait(Math.random() * 2000);
+    if (Math.random() > 0.5) {
+      return Promise.reject(new Error("foo"));
+    }
   };
   return (
     <>
+      <h3>Confirmation examples</h3>
+      <p>
+        A bunch of different confirm implementations using <code>Stateful</code>.
+      </p>
+
       <div className="buttons">
-        <Stateful confirm="onClick">
+        {/*
+          In this example, we just wrap a compatible ModalConfirm dialog
+          that supports onCancel and onConfirm props out-of-the-box.
+          Also, we do not specify any custom exit mode, hiding the Stateful.Confirm when the callback is invoked.
+        */}
+        <Stateful confirm>
           <button className="button" onClick={handleProceed}>
             Confirm (exit=started)
           </button>
           <Stateful.Confirm>
-            {({ onConfirm, onCancel }) => {
+            <ModalConfirm title="Default confirm">
+              <p>
+                This dialog will disappear immediatly when the user confirms.
+                <br />
+                All other states are handled by the original button.
+              </p>
+              <br />
+              <p>Do you want to proceed?</p>
+            </ModalConfirm>
+          </Stateful.Confirm>
+        </Stateful>
+
+        {/*
+          In this example, we again wrap a dialog, however, we manually assign
+          the names of the dialog props for confirming or aborting via "confirm" and "cancel" props. 
+          Also, we use exit mode "finished", hiding the Stateful.Confirm after the callback has finished.
+        */}
+        <Stateful confirm="onClick">
+          <button className="button" onClick={handleProceed}>
+            Confirm (exit=finished)
+          </button>
+          <Stateful.Confirm confirm="onConfirm" cancel="onCancel" exit="finished">
+            <ModalConfirm title="Confirm using exit=finished">
+              <p>This dialog will stay until the original callback has finished.</p>
+              <br />
+              <p>
+                The pending and busy states are handled inside this dialog itself.
+                <br />
+                However, the success and error states are still handled by the original button.
+              </p>
+              <br />
+              <p>Do you want to proceed?</p>
+            </ModalConfirm>
+          </Stateful.Confirm>
+        </Stateful>
+
+        {/*
+          In this example, we use a render function for confirmation. 
+
+          Also, we use exit="idle". However, this time we declare it
+          on the Stateful wrapper, where it's called "confirmExit" instead of just "exit"
+        */}
+        <Stateful confirm="onClick" confirmExit="idle">
+          <button className="button" onClick={handleProceed}>
+            Confirm (exit=idle)
+          </button>
+          <Stateful.Confirm>
+            {({ onCancel, onConfirm }) => {
               return (
                 <ModalConfirm
                   onConfirm={onConfirm}
                   onCancel={onCancel}
-                  title={`Confirm using exit="started" (default)`}
+                  title="Confirm using exit=idle"
                 >
-                  <p>This confirmation will disappear when the original callback has started.</p>
-                  <br />
                   <p>
-                    That way, the pending and busy states are handled by the original button,
-                    outside the confirmation window itself.
+                    This confirmation will disappear once the Stateful state switches back to idle.
                   </p>
                   <br />
+                  <p>
+                    That way, all states including error and success are handled inside the
+                    confirmation.
+                  </p>
+                  <p>
+                    Note that you would take extra steps to make sure the buttons are not clickable
+                    during error and success states.
+                  </p>
                   <br />
                   <p>Do you want to proceed?</p>
                 </ModalConfirm>
@@ -43,68 +106,43 @@ export default function ConfirmExample() {
           </Stateful.Confirm>
         </Stateful>
 
-        <Stateful confirm="onClick">
-          <button className="button" onClick={handleProceed}>
-            Confirm (exit=finished)
-          </button>
-          <Stateful.Confirm exit="finished">
-            <ModalConfirm title={`Confirm using exit="finished"`}>
-              <p>This confirmation will disappear when the original callback has finished.</p>
-              <br />
-              <p>
-                That way, the pending and busy states need to be handled inside the confirmation
-                window itself.
-              </p>
-              <p>The success and error states are still handled by the original button.</p>
-              <br />
-              <p>Do you want to proceed?</p>
-            </ModalConfirm>
-          </Stateful.Confirm>
-        </Stateful>
-
-        <Stateful confirm="onClick">
-          <button className="button" onClick={handleProceed}>
-            Confirm (exit=idle)
-          </button>
-          <Stateful.Confirm exit="idle">
-            <ModalConfirm title={`Confirm using exit="idle"`}>
-              <p>
-                This confirmation will disappear once the Stateful state switches back to idle.
-              </p>
-              <br />
-              <p>
-                That way, all states including error and success are handled inside the confirmation.
-              </p>
-              <p>
-                Note that you would take extra steps to make sure the buttons are not clickable during error and success states.
-              </p>
-              <br />
-              <p>Do you want to proceed?</p>
-            </ModalConfirm>
-          </Stateful.Confirm>
-        </Stateful>
-
-        <Stateful confirm="onClick" confirmClasses="is-warning">
+        {/*
+          In this example, we build an inline confirmation without using a modal dialog at all.
+          We use a render function to have completely different components depending on the status.
+        */}
+        <Stateful confirm>
           {({ status, handlers }) => {
-            if (status === Status.CONFIRM)
+            if (status !== Status.CONFIRM) {
+              // the regular button
               return (
-                <div className="field has-addons">
-                  <div className="control">
-                    <button className="button" onClick={handlers.onConfirmCancel}>
-                      Cancel
-                    </button>
-                  </div>
-                  <div className="control">
-                    <button className="button is-primary" onClick={handlers.onConfirmApprove}>
-                      Confirm
-                    </button>
-                  </div>
-                </div>
+                <button className="button" onClick={handleProceed}>
+                  Confirm inline
+                </button>
               );
+            }
+            // confirm state with different components
             return (
-              <button className="button" onClick={handleProceed}>
-                Confirm inline
-              </button>
+              <div className="field has-addons">
+                <div className="control">
+                  <button className="button" onClick={handlers.onConfirmCancel}>
+                    <span className="icon is-small">
+                      <i className="fas fa-times"></i>
+                    </span>
+                  </button>
+                </div>
+                <div className="control">
+                  <button className="button" style={{ pointerEvents: "none" }}>
+                    Sure?
+                  </button>
+                </div>
+                <div className="control">
+                  <button className="button is-primary" onClick={handlers.onConfirmApprove}>
+                    <span className="icon is-small">
+                      <i className="fas fa-check"></i>
+                    </span>
+                  </button>
+                </div>
+              </div>
             );
           }}
         </Stateful>
@@ -116,7 +154,7 @@ export default function ConfirmExample() {
 }
 
 type ModalConfirmProps = {
-  title: string;
+  title?: string;
   // onCancel and onConfirm are supported out of the box
   onCancel?: () => void;
   onConfirm?: () => void;
@@ -126,10 +164,12 @@ function ModalConfirm(props: React.PropsWithChildren<ModalConfirmProps>) {
     <div className="modal is-active">
       <div className="modal-background"></div>
       <div className="modal-card">
-        <header className="modal-card-head">
-          <div className="modal-card-title">{props.title}</div>
-          <button className="delete" aria-label="close" onClick={props.onCancel}></button>
-        </header>
+        {props.title && (
+          <header className="modal-card-head">
+            <div className="modal-card-title">{props.title}</div>
+            <button className="delete" aria-label="close" onClick={props.onCancel}></button>
+          </header>
+        )}
         <section className="modal-card-body">{props.children}</section>
         <footer className="modal-card-foot">
           <button className="button" onClick={props.onCancel}>
